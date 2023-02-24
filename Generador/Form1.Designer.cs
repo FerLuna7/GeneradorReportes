fcaa6943609bb6009bbf8009e21c94e756dfbc8a
FerLuna7 <fernando.luna@focusonservices.com>
2023-02-24 17:24:48 -0600

    Agregar archivos de proyecto.

---
+++ Generador/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Generador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(269, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Crear Reporte";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(19, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(244, 199);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(7, 138);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(439, 238);
+            this.listBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(324, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Seleccionar arhivo 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(311, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(324, 53);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Seleccionar archivo 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(311, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(324, 88);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 44);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Comparar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 237);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(402, 227);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Generar Reporte";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox2);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(441, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(455, 385);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Comparar archivo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(920, 476);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Generador/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+using ExcelDataReader;
+using OfficeOpenXml;
+
+namespace Generador
+{
+    public partial class Form1 : Form
+    {
+        static Form1()
+        {
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Mostrar el cuadro de diálogo del explorador de carpetas
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string folderPath = folderBrowserDialog1.SelectedPath;
+
+                // Configurar el objeto de aplicación de Excel
+                Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+                excelApp.Visible = true;
+                Workbook excelWorkbook = excelApp.Workbooks.Add();
+                Worksheet excelWorksheet = (Worksheet)excelWorkbook.Worksheets[1];
+                excelWorksheet.Cells[1, 1] = "Folio";
+                excelWorksheet.Cells[1, 2] = "Serie";
+                excelWorksheet.Cells[1, 3] = "Total";
+                excelWorksheet.Cells[1, 4] = "Fecha";
+                excelWorksheet.Cells[1, 5] = "UUID";
+                int rowNum = 2;
+
+                // Buscar archivos .xml en la carpeta y subcarpetas
+                string[] files = Directory.GetFiles(folderPath, "*.xml", SearchOption.AllDirectories);
+                foreach (string file in files)
+                {
+                    // Cargar el archivo .xml en un objeto XmlDocument
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.Load(file);
+
+                    // Obtener la información del cfdi:Comprobante y el cfdi:Complemento
+                    XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmlDoc.NameTable);
+                    nsmgr.AddNamespace("cfdi", "http://www.sat.gob.mx/cfd/3");
+                    XmlNode comprobanteNode = xmlDoc.SelectSingleNode("//cfdi:Comprobante", nsmgr);
+                    string folio = comprobanteNode.Attributes["Folio"].Value;
+                    string serie = comprobanteNode.Attributes["Serie"].Value;
+                    string total = comprobanteNode.Attributes["Total"].Value;
+                    string fecha = comprobanteNode.Attributes["Fecha"].Value;
+                    XmlNode complementoNode = xmlDoc.SelectSingleNode("//cfdi:Complemento", nsmgr);
+                    string uuid = complementoNode.FirstChild.Attributes["UUID"].Value;
+
+                    // Agregar la información al informe de Excel y al ListBox
+                    excelWorksheet.Cells[rowNum, 1] = folio;
+                    excelWorksheet.Cells[rowNum, 2] = serie;
+                    excelWorksheet.Cells[rowNum, 3] = total;
+                    excelWorksheet.Cells[rowNum, 4] = fecha;
+                    excelWorksheet.Cells[rowNum, 5] = uuid;
+                    listBox1.Items.Add(file);
+
+                    rowNum++;
+                }
+
+
+                // Guardar y cerrar el archivo de Excel
+                string fileName = "Informe.xml.xlsx";
+                string filePath = Path.Combine(folderPath, fileName);
+                excelWorkbook.SaveAs(filePath);
+                excelWorkbook.Close();
+                excelApp.Quit();
+                MessageBox.Show("Informe guardado en " + filePath);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Archivos de Excel (*.xlsx)|*.xlsx|Todos los archivos (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog2 = new OpenFileDialog();
+            openFileDialog2.Filter = "Archivos de Excel (*.xlsx)|*.xlsx|Todos los archivos (*.*)|*.*";
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = openFileDialog2.FileName;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear(); //Limpiar ListBox antes de mostrar las coincidencias
+
+            //Obtener el contenido de la columna "UUID" del primer archivo
+            string[] uuids = File.ReadAllLines(textBox1.Text);
+            string[] uuidsArray = new string[uuids.Length - 1]; //Crear arreglo sin la primera línea (encabezado)
+            Array.Copy(uuids, 1, uuidsArray, 0, uuids.Length - 1); //Copiar el contenido sin la primera línea
+            string uuidsTexto = String.Join(",", uuidsArray); //Convertir a texto separado por comas para usarlo en el método Contains
+
+            //Obtener el contenido de la columna "TIMBRE" del segundo archivo
+            string[] timbres = File.ReadAllLines(textBox2.Text);
+            string[] timbresArray = new string[timbres.Length - 1]; //Crear arreglo sin la primera línea (encabezado)
+            Array.Copy(timbres, 1, timbresArray, 0, timbres.Length - 1); //Copiar el contenido sin la primera línea
+
+            //Recorrer cada línea del segundo archivo y buscar coincidencias en el primer archivo
+            for (int i = 0; i < timbresArray.Length; i++)
+            {
+                if (uuidsTexto.Contains(timbresArray[i].Split(',')[0])) //Buscar el UUID en la columna TIMBRE del segundo archivo
+                {
+                    listBox2.Items.Add($"Coincidencia en línea {i + 2} del archivo {textBox2.Text}"); //Agregar la coincidencia al ListBox
+                }
+            }
+           
+        }
+
+        private System.Data.DataTable ReadExcelFile(string filePath)
+        {
+            System.Data.DataTable dt = new System.Data.DataTable();
+
+            try
+            {
+                using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    using (var reader = ExcelReaderFactory.CreateReader(stream))
+                    {
+                        while (reader.Read())
+                        {
+                            DataRow row = dt.NewRow();
+                            for (int i = 5; i < reader.FieldCount; i++)
+                            {
+                                row[i] = reader.GetValue(i);
+                            }
+                            dt.Rows.Add(row);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return dt;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string nombreArchivo = textBox3.Text;
+            string directorio = @"C:\";
+            try
+            {
+
+                string[] archivos = Directory.GetFiles(directorio, nombreArchivo, SearchOption.AllDirectories);
+
+                listBox3.Items.Clear();
+                foreach (string archivo in archivos)
+                {
+                    listBox3.Items.Add(archivo);
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"No se pudo acceder a la carpeta: {ex.Message}", "Error de acceso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+
